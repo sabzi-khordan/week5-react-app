6e0cc93e6cc99fc4b19cf7006a166b0a638aadda
sabzi-khordan <akram.babaei94@yahoo.com>
2023-05-13 21:02:40 +0330

    Added bootsrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App" >
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
-      
+        This project was coded by Akiya and is <a href="https://github.com/sabzi-khordan/week5-react-app" target="_blank"> 
+          open-sourced
+        </a>
       </footer>
+      </div>
     </div>
   )
     
