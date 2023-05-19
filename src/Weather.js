d0bf13fe3584ded065cbf751d3d787685e5a4cad
sabzi-khordan <akram.babaei94@yahoo.com>
2023-05-19 21:49:42 +0330

    Forecast/API

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React ,{useState} from "react";
 import axios from "axios";
+import WeatherForecast from"./WeatherForecast";
 import "./Weather.css";
 
 import WeatherInfo from "./WeatherInfo";
@@ -12,6 +13,7 @@ export default function  Weather(props){
         console.log(response.data);
         setWeatherData({
             ready: true,
+            coordinates: response.data.coord,
             temperature:response.data.main.temp,
             humidity:response.data.main.humidity,
             date: new Date(response.data.dt*1000),
@@ -57,6 +59,7 @@ export default function  Weather(props){
                 </div>
             </form>
             <WeatherInfo  data={weatherData}/>
+            <WeatherForecast   coordinates={weatherData.coordinates}/>
             
         </div>
     )
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -29,7 +29,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="red"
-      size={52}
+      size={props.size}
       animate={true}
     />
   );
