b44089648664a696ea36dc461dd455b297959779
sabzi-khordan <akram.babaei94@yahoo.com>
2023-05-19 17:52:20 +0330

    add unit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 
 export default function WeatherInfo(props) {
@@ -18,11 +19,12 @@ export default function WeatherInfo(props) {
             <div className="row">
                 <div className="col-6     text-start" >
                   <div className="float-left">
-                  <WeatherIcon code={props.data.icon}   />
+                    <WeatherIcon code={props.data.icon}   />
                   </div>
-                    
-                    <span className="temperature"> {Math.round(props.data.temperature)}</span>
-                    <span className="unit">℃</span>
+                    <div className="float-left">
+                      <WeatherTemperature  celsius={props.data.temperature}/>
+                      
+                    </div>
                 </div>
                 <div className="col-6">
                     <ul>
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React ,{useState} from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
-    </div>
-  );
+  const [unit,setUnit]=useState("celsius");
+
+  function convertToFahrenheit(event){
+    event.preventDefault();
+    setUnit("fahrenheit")
+  }
+
+  function convertToCelsius(event){
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+
+  if(unit===`celsius`){
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">°C | <a href="/"  onClick={convertToFahrenheit}  rel="noreferrer"  >℉ </a></span>
+        
+      </div>
+    );
+
+  }else{
+    let farenheit= (props.celsius*9/5)+32;
+    return(
+       <div className="WeatherTemperature">
+          <span className="temperature">{Math.round(farenheit)}</span>
+          <span className="unit">  <a href="/"  onClick={convertToCelsius}   rel="noreferrer" >    °C   </a>| ℉ </span>
+    
+      </div>
+          );
+  }
+
+
+
+ 
 }
+
